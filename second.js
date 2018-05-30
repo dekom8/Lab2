1d6ce495089f355e19060df69995afa447e974df
dekom8 <38499476+dekom8@users.noreply.github.com>
2018-05-30 20:42:42 +0300

    Add files via upload

---
+++ second.js
@@ -0,0 +1,36 @@
+jQuery(".redLight").css("box-shadow", "0px 0px 50px 20px rgb(144,0,0)");
+$("#yellowCar").css("margin-right", "-200px");
+$("#yellowCar").animate({
+  left: "-800px"
+}, 3000);
+
+$(".greenLight").click(function() {
+  
+  var msg = $("h2").text("Yay!");
+  var carMove = $("#yellowCar");
+  var redLightOff = $(".redLight");
+  var greenLightOn = $(".greenLight").css("box-shadow", "0px 0px 50px 20px rgb(0,110,0)");
+
+  redLightOff.css("box-shadow", "0px 0px 0px 0px");
+
+  carMove.animate({
+    left: "-2000px"
+  }, 4000);
+
+  carMove.animate({
+    left: "800px"
+  }, 1);
+
+});
+
+$("#retryBtn").click(function() {
+  
+  var msg = $("h2").text('Press "Green light" to go!');
+  
+  $(".greenLight").css("box-shadow", "0px 0px 0px 0px");
+  $(".redLight").css("box-shadow", "0px 0px 50px 20px rgb(144,0,0)");
+  $("#yellowCar").css("margin-right", "-200px");
+  $("#yellowCar").animate({
+    left: "-800px"
+  }, 5000);
+});
